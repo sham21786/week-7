116577da7a33ed8ac8eec4c0cfcac41bbb86a5ed
Shazmil Nassar <sham21786@yahoo.com>
2015-05-08 15:14:00 +0100

    week 7

---
+++ Assets/alter_myShader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class alter_myShader : MonoBehaviour {
+
+    public Renderer rend;
+
+	void Update () 
+    {
+        rend = GetComponent<Renderer>();
+        Color ColourA = Color.blue;
+        Color ColourB = Color.yellow;
+        ColourA.r = Mathf.PingPong(Time.time, 1f);
+        ColourB.r = Mathf.PingPong(Time.time, 1f);
+
+        var rnd = Random.Range(0, 5);
+        if (rnd == 0)
+        {
+            rend.material.SetColor("_Color", ColourA);
+        }
+        else if (rnd == 4){
+            rend.material.SetColor("_Color", ColourB);
+        }
+	}
+}
